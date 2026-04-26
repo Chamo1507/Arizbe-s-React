cd11c76da578c544297a84e4105c8d7c4585f914
Moisés Medina <px4webs@gmail.com>
2026-04-25 19:52:46 -0600

    Agregar componente PHFormulario

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,2 +1,8 @@
-const Contacto = () => {};
+import PHFormulario from "../components/PHFormulario/PHFormulario";
+import "./Contacto.css";
+
+const Contacto = () => {
+  return <PHFormulario />;
+};
+
 export default Contacto;
