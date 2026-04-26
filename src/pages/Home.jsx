80c499c181794a6e0e94d2b633546e34b2c647a4
Imanol Aguilar <imanolaguilare@gmail.com>
2026-04-25 20:35:02 -0600

    Estructura completa de Inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,73 @@
 import "./pages.css";
+import "./home.css";
+//import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <div className="mainContainer">
-      <img
-        id="BackInicio"
-        src="https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUyNDR1ZnNneW56Ym1iNmxkZ3lnNGM0bDV0NzBkNjI0NDJzd3o1eTMzeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ovvRlfxOfy5PvAPdTo/giphy.gif"
-        alt="Fondo de inicio"
-      />
+    <div>
+      <div className="mainContainer">
+        <img
+          id="BackInicio"
+          src="https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUyNDR1ZnNneW56Ym1iNmxkZ3lnNGM0bDV0NzBkNjI0NDJzd3o1eTMzeCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/ovvRlfxOfy5PvAPdTo/giphy.gif"
+          alt="Background Inicio"
+        />
 
-      <main>
-        <div className="container">
-          <img id="logoInicio" src="" alt="Logo Arizbes" />
-        </div>
-      </main>
+        <main>
+          <div className="container">
+            <img id="logoInicio" src="" alt="Logo Arizbes" />
+          </div>
+        </main>
+      </div>
 
+      {/* Seccion Nosotros */}
       <div className="secciones" id="PocoDeNosotros">
         <div className="PocoNosotrosText">
-          <h1>Un poco de nosotros</h1>
+          <h1>Un poco de nososotros</h1>
           <h2>
             Elaboramos nuestros productos con ingredientes de calidad, procesos
             tradicionales y el cuidado que solo el trabajo hecho a mano puede
-            ofrecer.
+            ofrecer. Nuestro objetivo es compartir sabores auténticos que
+            recuerdan a lo casero, creando un espacio donde cada cliente pueda
+            disfrutar del aroma, la calidez y la dedicación que ponemos en cada
+            pieza recién horneada.
           </h2>
         </div>
         <div className="IMGPocoDeNosotros">
-          <img src="" alt="Pan 1" />
-          <img src="" alt="Pan 2" />
-          <img src="" alt="Pan 3" />
-          <img src="" alt="Pan 4" />
+          <img src="" alt="Rol 1" />
+          <img src="" alt="Rol 2" />
+          <img src="" alt="Rol 3" />
+          <img src="" alt="Rol 6" />
         </div>
       </div>
 
+      {/* Seccion Banners */}
       <div className="secciones" id="BannerContainer">
         <div>
-          <Link to="/productos#panaderia">
-            <img src="" alt="Icono Panadería" />
-            <h1>Panadería</h1>
+          <a href="#panaderia">
+            <img src="" alt="Icono Panaderia" />
+            <h1>Panaderia</h1>
             <h2>Ver más</h2>
-          </Link>
+          </a>
         </div>
         <div>
-          <Link to="/productos#postres">
+          <a href="#postres">
             <img src="" alt="Icono Postres" />
             <h1>Postres</h1>
             <h2>Ver más</h2>
-          </Link>
+          </a>
         </div>
         <div>
-          <Link to="/productos#especiales">
+          <a href="#especiales">
             <img src="" alt="Icono Especiales" />
             <h1>Especiales de la casa</h1>
             <h2>Ver más</h2>
-          </Link>
+          </a>
         </div>
       </div>
 
+      {/* Galeria */}
       <div className="secciones" id="Galeria">
         <div className="GaleriaText">
-          <h1> GALERÍA </h1>
+          <h1>-- GALERIA --</h1>
         </div>
         <div className="GaleriaContainer"></div>
       </div>
