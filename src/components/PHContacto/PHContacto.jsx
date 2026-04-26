c0eb85c28341e63ed8287c103a68f7815251ea01
Moisés Medina <px4webs@gmail.com>
2026-04-26 17:17:13 -0600

    ya añadi el codigo completo de ambos archivos de PHCntacto

--- src/components/PHContacto/PHContacto.jsx
+++ src/components/PHContacto/PHContacto.jsx
@@ -1,1 +1,21 @@
 import "./PHContacto.css";
+
+const PHContacto = () => {
+  return (
+    <section className="ph-contacto">
+      <div className="ph-contacto-introduccion">
+        <h1>Contacto</h1>
+
+        <p>
+          En Arizbe&apos;s nos importa que tu pedido quede lo más perfecto
+          posible, por lo que si tienes un pedido personalizado o tienes un
+          evento importante
+        </p>
+
+        <h3>¡No dudes en contactarnos!</h3>
+      </div>
+    </section>
+  );
+};
+
+export default PHContacto;
