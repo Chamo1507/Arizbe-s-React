fe6b5968645cb651fb9a843f756cff0cc8414960
Samuel Ramirez <samuel_ramirezbueno@hotmail.com>
2026-04-26 22:07:46 -0600

    refactor(input):Creacion del componente

--- src/components/CInput/CInput.jsx
+++ src/components/CInput/CInput.jsx
@@ -1,5 +1,8 @@
 import "./CInput.css";
-const CInput = () => {
-  return <div></div>;
+const CInput = ({ lbNombre, ph, nombre, tipo, className = "" }) => {
+  <div className={`cinput ${className}`.trim()}>
+    <label htmlFor="nombre">{lbNombre}</label>
+    <input type={tipo} id={nombre} name={nombre} placeholder={ph} required />
+  </div>;
 };
 export default CInput;
