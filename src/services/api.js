f0f69752822a043dfad1b9f21de871fb76aa19bc
Samuel Ramirez <samuel_ramirezbueno@hotmail.com>
2026-05-18 05:25:48 -0600

    feat(api): replace port 3000 to 3001

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000", // La URL de tu NestJS
+  baseURL: "http://localhost:3001", // La URL de tu NestJS
   headers: {
     "Content-Type": "application/json",
   },
